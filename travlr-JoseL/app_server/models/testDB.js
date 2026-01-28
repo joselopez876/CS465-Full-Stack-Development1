d8d09e53785440c3d0def402d2737e83fe940002
Jose Lopez <jose.lopez24@snhu.edu>
2026-01-27 23:14:49 -0700

    Module 4 completed baseline

---
+++ travlr-JoseL/app_server/models/testDB.js
@@ -0,0 +1,10 @@
+const mongoose = require('./db');
+
+mongoose.connection.on('connected', () => {
+  console.log("✅ MongoDB connected successfully");
+  process.exit(0);
+});
+
+mongoose.connection.on('error', err => {
+  console.log("❌ MongoDB connection error:", err);
+});
