5d8f2c663bbe0f7e19df25d32b8ec4669780982a
Jose Lopez <jose.lopez24@snhu.edu>
2026-02-15 05:56:49 -0700

    Module 6 completed baseline

--- travlr-JoseL/app_api/routes/index.js
+++ travlr-JoseL/app_api/routes/index.js
@@ -2,8 +2,11 @@ const express = require('express');
 const router = express.Router();
 const tripsController = require('../controllers/trips');
 
-router.route('/trips').get(tripsController.tripsList);
-router.route('/trips/:tripCode').get(tripsController.tripsFindByCode);
+router.route('/trips').get(tripsController.tripsList)// GET method gets all trips
+    .post(tripsController.tripsAddTrip) // POST method adds a trip
+
+router.route('/trips/:tripCode').get(tripsController.tripsFindByCode)
+    .put(tripsController.tripsUpdateTrip);// PUT method updates trip;
 
 module.exports = router;
 
